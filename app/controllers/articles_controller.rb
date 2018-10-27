3a9f6a22946e1bc5d9fa46a1849ee72a81e03582
Marcos Soares <mavmaso@gmail.com>
2018-10-27 20:35:21 -0300

    Refactoring with set_article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
-class ArticlesController < ApplicationController 
+class ArticlesController < ApplicationController
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
 
   def index
     @articles = Article.all
@@ -18,12 +19,9 @@ class ArticlesController < ApplicationController
     end
   end
 
-  def edit
-    @article = Article.find(params[:id])
-  end
+  def edit; end
 
   def update
-    @article = Article.find(params[:id])
     if @article.update(article_params)
       redirect_to article_path(@article), notice: 'Artigo atualizado'
     else
@@ -31,17 +29,18 @@ class ArticlesController < ApplicationController
     end
   end
 
-  def show
-    @article = Article.find(params[:id])
-  end
+  def show; end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     redirect_to articles_path, notice: 'Apagado com sucesso'
   end
 
   private
+
+    def set_article
+      @article = Article.find(params[:id])
+    end
 
     def article_params
       params.require(:article).permit(:title, :description)
