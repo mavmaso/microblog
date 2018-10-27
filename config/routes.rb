a80f9a981463f51f514e14415137cd3b1858eb5f
Marcos Soares <mavmaso@gmail.com>
2018-10-27 19:09:02 -0300

    Fix git ignore and adding edit and update

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   root to: 'home#index'
-  resources :articles, only: [:index, :new, :create, :show]
+  resources :articles, only: [:index, :new, :create, :show, :edit, :update]
 end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
+  
 end
