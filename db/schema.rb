b957d03eb1228ac084ce0ece81a8431ca317f03a
Marcos Soares <mavmaso@gmail.com>
2019-05-09 14:48:16 -0300

    add secure password to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_09_132627) do
+ActiveRecord::Schema.define(version: 2019_05_09_173826) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -21,11 +21,7 @@ ActiveRecord::Schema.define(version: 2019_05_09_132627) do
     t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+# Could not dump table "users" because of following StandardError
+#   Unknown type 'strings' for column 'password_digest'
 
 end
