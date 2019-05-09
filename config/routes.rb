2754ca1f4758f6aaff17be1d85c707fe497021d6
Marcos Soares <mavmaso@gmail.com>
2019-05-09 17:21:24 -0300

    adding user signup template

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root to: 'home#index'
   resources :articles, only: [:index, :new, :create, :show, :edit, :update, :destroy]
+  get '/signup', to: 'users#new'
+  resources :users, except: [:new], only: [:create, :edit, :update]
 end
