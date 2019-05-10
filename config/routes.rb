31df943f6afda6df4c6e718b59b110a15b61dace
Marcos Soares <mavmaso@gmail.com>
2019-05-10 17:09:27 -0300

    Create show action and add gravatar

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root to: 'home#index'
   resources :articles, only: [:index, :new, :create, :show, :edit, :update, :destroy]
   get '/signup', to: 'users#new'
-  resources :users, except: [:new], only: [:create, :edit, :update]
+  resources :users, except: [:new], only: [:create, :edit, :update, :show]
 end
