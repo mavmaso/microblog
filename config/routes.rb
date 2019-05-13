403176c39ae92c3eabf1e7ae4467ef70ae5e0093
Marcos Soares <mavmaso@gmail.com>
2019-05-13 17:30:01 -0300

    login form

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
   resources :articles, only: [:index, :new, :create, :show, :edit, :update, :destroy]
   get '/signup', to: 'users#new'
   resources :users, except: [:new], only: [:create, :edit, :update, :show, :index]
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
 end
