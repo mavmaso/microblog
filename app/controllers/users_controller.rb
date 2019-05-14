566ca06214e9f00e04da71a9880ca332608a3966
Marcos Soares <mavmaso@gmail.com>
2019-05-13 22:17:12 -0300

    add user authentication methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update, :show]
+
   def index
     @users = User.paginate(page: params[:page], per_page: 4)
   end
@@ -18,11 +20,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:success] = 'Sua conta foi atualizada'
       redirect_to root_path
@@ -32,7 +32,6 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
     @user_articles = @user.articles.paginate(page: params[:page], per_page: 3)
   end
 
@@ -40,5 +39,9 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
