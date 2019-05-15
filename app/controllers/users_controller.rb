e5485d73388ec4a1e6eeef1a4d0ca085ceea8283
Marcos Soares <mavmaso@gmail.com>
2019-05-15 10:18:55 -0300

    Add admin user powers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:edit, :update, :show]
-  before_action :require_same_user, only: [:edit, :update]
+  before_action :set_user, only: [:edit, :update, :show, :destroy]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
+  before_action :require_admin, only: [:destroy]
 
   def index
     @users = User.paginate(page: params[:page], per_page: 4)
@@ -37,6 +38,12 @@ class UsersController < ApplicationController
     @user_articles = @user.articles.paginate(page: params[:page], per_page: 3)
   end
 
+  def destroy
+    @user.destroy
+    flash[:danger] = 'Usuario e seus artigos foram todos apagados'
+    redirect_to users_path
+  end
+
   private
 
   def user_params
@@ -50,6 +57,13 @@ class UsersController < ApplicationController
   def require_same_user
     if current_user != @user and !current_user.admin?
       flash[:danger] = 'Você não tem essa permissão'
+      redirect_to root_path
+    end
+  end
+
+  def require_admin
+    if logged_in? and !current_user.admin?
+      flash[:danger] = 'Somente admin pode fazer isso'
       redirect_to root_path
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: 'home#index'
   resources :articles, only: [:index, :new, :create, :show, :edit, :update, :destroy]
   get '/signup', to: 'users#new'
-  resources :users, except: [:new], only: [:create, :edit, :update, :show, :index]
+  resources :users, except: [:new], only: [:create, :edit, :update, :show, :index, :destroy]
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
