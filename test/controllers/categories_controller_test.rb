b7ed570058cf7883e6c74253b036d8949194d68f
Marcos Soares <mavmaso@gmail.com>
2019-05-15 15:51:54 -0300

    [WIP] test categories

---
+++ test/controllers/categories_controller_test.rb
@@ -0,0 +1,18 @@
+require 'test_helper'
+
+class CategoriesControllerTest < ActionDispatch::IntegrationTest
+  test 'should get categories index' do
+    get categories_path
+    assert_response :success
+  end
+
+  test 'should get new' do
+    get new_category_path
+    assert_response :success
+  end
+
+  test 'should get show' do
+    get category_path
+    assert_response :success
+  end
+end
